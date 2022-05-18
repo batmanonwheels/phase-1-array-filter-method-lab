25c438bcb0de7ef65ff9b0916a13fef9321196ed
Devin <devingarcia5423@gmail.com>
2022-05-18 11:39:07 -0400

    commit tree c39dbc480a6faab80b2492e3b04057de7b3cf658

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-// Code your solution here
+//Return drivers that match the input name
+
+function findMatching(array, str) {
+    let match = array.filter(driverNames => driverNames.toLowerCase() === str.toLowerCase())
+    return match;
+}
+
+function fuzzyMatch(array, str) {
+    let match = array.filter(driverNames => driverNames.startsWith(str))
+    return match;
+}
+
+function matchName(array, str) {
+    let match = array.filter(driverNames => driverNames.name === str)
+    return match;
+}
